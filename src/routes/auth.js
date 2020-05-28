c30803fc953942e94f2a1c1a144b516a93b65b8b
danang <danangekoyudanto1995@gmail.com>
2020-05-28 19:04:52 +0700

    login endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -13,6 +13,7 @@ const {
 } = require('@utils/token');
 const { sendActivationEmail } = require('@utils/emails');
 const Students = require('@schema/students');
+const Tutors = require('@schema/tutors');
 const router = express.Router();
 
 const DateNow = moment().format('MM/DD/YYYY, HH:mm:ss');
@@ -85,7 +86,57 @@ router.post(
     }
   }
 );
-router.post('/login', [], async (req, res) => {});
+router.post(
+  '/login',
+  [
+    body('email', 'email should be present').exists(),
+    body('password', 'passwords should be present').exists(),
+    body('type', 'type should be present').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json(response(422, errors.array()));
+    }
+
+    const { email, password, type, remember_me } = req.body;
+    try {
+      let user;
+      if (type === 'student') {
+        user = await Students.findOne({ email });
+      } else if (type === 'tutor') {
+        user = await Tutors.findOne({ email });
+      }
+
+      if (!user) {
+        return res.status(400).json(response(400, 'User not found!'));
+      }
+
+      const compare = encrypt(password) === user.password;
+      if (!compare) {
+        return res.status(400).json(response(400, 'Password salah!'));
+      }
+
+      const token = await getToken({ uid: user._id, rememberMe: remember_me, type });
+      let getExpToken = await getPayload(token.pure);
+
+      const payload = Object.freeze({
+        token: { key: token.key, exp: getExpToken.exp },
+        user: {
+          id: user._id,
+          name: user.full_name,
+          email: user.email,
+          phone: user.phone ? user.phone : null,
+        },
+        type,
+      });
+
+      return res.status(200).json(response(200, 'Login berhasil', payload));
+    } catch (error) {
+      return res.status(500).json(response(500, 'Internal Server Error!', error));
+    }
+  }
+);
 
 router.get(
   '/confirm-email',
