5ac549871890d47c2bd087027c7ac1711344456f
danang <danangekoyudanto1995@gmail.com>
2020-06-09 23:51:52 +0700

    make it better

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const app = express();
 const helmet = require('helmet');
 const cors = require('cors');
 const RateLimit = require('express-rate-limit');
-const config = require('@config');
 
 const limitedAccess = new RateLimit({
   windowMs: 1 * 60 * 1000, // 15 minutes
@@ -21,9 +20,6 @@ app.disable('etag');
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-app.use('/uploads', express.static(config.uploads));
-app.use('/documents', express.static(config.documents));
 
 app.use('/auth', limitedAccess, require('./routes/auth'));
 
